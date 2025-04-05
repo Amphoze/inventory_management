a359cde364b0434bf19395591d0c5230f445de73
anupsoni592004 <anupkumarsoni@katyayaniorganics.com>
2025-04-05 19:14:22 +0530

    Update version_controller.dart

--- lib/stockship_version_control/version_controller.dart
+++ lib/stockship_version_control/version_controller.dart
@@ -13,7 +13,7 @@ class VersionController extends ChangeNotifier {
   /// 3. firebase deploy                                              ///
   /// 4. Change version in firebase to this currentVersion.           ///
   ///////////////////////////////////////////////////////////////////////
-  String currentVersion = '6.8.6';
+  String currentVersion = '6.8.7';
   String? latestVersion = '';
 
   void setLatest(String value) {
